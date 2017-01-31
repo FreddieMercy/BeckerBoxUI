d579fb74a56cc0a048996edc413c6e73bbb7766a
jzhang1 <junhao.zhang.freddie@gmail.com>
2017-01-31 07:47:12 -0800

    ScrollViewerHelper added, now can Scroll to the end

---
+++ BeckerBox_KeyboardAndGazePoint/SightSign/CS/ScrollViewerHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using Microsoft.HandsFree.Mouse;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using EyeXFramework;
+using System.Collections.ObjectModel;
+using System.Collections;
+
+
+namespace BeckerBox
+{
+    public static class Helper
+    {
+        public static bool GetAutoScroll(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutoScrollProperty);
+        }
+
+        public static void SetAutoScroll(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoScrollProperty, value);
+        }
+
+        public static readonly DependencyProperty AutoScrollProperty =
+            DependencyProperty.RegisterAttached("AutoScroll", typeof(bool), typeof(Helper), new PropertyMetadata(false, AutoScrollPropertyChanged));
+
+        private static void AutoScrollPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var scrollViewer = d as ScrollViewer;
+
+            if (scrollViewer != null && (bool)e.NewValue)
+            {
+                scrollViewer.ScrollToBottom();
+            }
+        }
+    }
+}
